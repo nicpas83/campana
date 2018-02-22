ef7d8a0590cd6e03134d3fd1fedd254fc9299174
nicpas <nicpas@gmail.com>
2018-02-22 16:08:03 -0300

    no message

---
+++ app/Plugin/Reclamos/Model/RelevamientoArbolado.php
@@ -0,0 +1,19 @@
+<?php
+
+class RelevamientoArbolado extends AppModel {
+
+    public $label = 'Relevamientos';
+    public $tablePrefix = 'rec_';
+    public $useTable = 'relevamientos_arbolado';
+    public $plugin = 'Reclamos';
+    
+
+    public $belongsTo = [
+        'Reclamo' => [
+            'className' => 'Reclamos.Reclamo',
+            'foreignKey' => 'reclamo_id',
+        ],
+    ];
+    
+
+}
---
+++ app/Plugin/Reclamos/Model/RelevamientoBacheo.php
@@ -0,0 +1,19 @@
+<?php
+
+class Relevamiento extends AppModel {
+
+    public $label = 'Relevamientos';
+    public $tablePrefix = 'rec_';
+    public $useTable = 'relevamientos';
+    public $plugin = 'Reclamos';
+    
+
+    public $belongsTo = [
+        'Reclamo' => [
+            'className' => 'Reclamos.Reclamo',
+            'foreignKey' => 'reclamo_id',
+        ],
+    ];
+    
+
+}
